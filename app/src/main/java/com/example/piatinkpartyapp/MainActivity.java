7092ec54664117ddb64fa66c1f8b58f76850a67d
tinesime <tine.simenc1@gmail.com>
2022-04-14 19:13:18 +0200

    lobby

--- app/src/main/java/com/example/piatinkpartyapp/MainActivity.java
+++ app/src/main/java/com/example/piatinkpartyapp/MainActivity.java
@@ -9,22 +9,45 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button BtnLobbyPlay;
+    private Button createGame;
+    private Button einstellungen;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
         //Calling the Lobby Activity
-        Button BtnLobbyPlay = (Button) findViewById(R. id.BtnPlayGame);
+        BtnLobbyPlay = (Button) findViewById(R. id.BtnPlayGame);
 
         BtnLobbyPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, LobbyScreen.class));
+                startActivity(new Intent(MainActivity.this, PlaySelectedGame.class));
                 finish();
             }
         });
+
+        // Calling the create game activity
+        createGame = (Button) findViewById(R.id.BtnCreateGame);
+
+        createGame.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, CreateGame.class));
+                finish();
+            }
+        }));
+
+        einstellungen = (Button) findViewById(R.id.BtnOptions);
+
+        einstellungen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Einstellungen.class));
+            }
+        });
+
     }
 }
