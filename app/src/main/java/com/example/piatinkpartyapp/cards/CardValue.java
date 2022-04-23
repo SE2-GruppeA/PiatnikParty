03a5af80ed851723f8eef34670c7cb5329846336
Alina <alinaeh@edu.aau.at>
2022-04-23 12:43:07 +0200

    Card-Values implemented

--- app/src/main/java/com/example/piatinkpartyapp/cards/CardValue.java
+++ app/src/main/java/com/example/piatinkpartyapp/cards/CardValue.java
@@ -1,4 +1,15 @@
 package com.example.piatinkpartyapp.cards;
-
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+/*Value a card can have*/
 public enum CardValue {
+    SIEBEN,
+    ACHT,
+    NEUN,
+    ZEHN,
+    UNTER,
+    OBER,
+    KOENIG, //"Ö" not possible due to resource accessing
+    ASS;
 }
