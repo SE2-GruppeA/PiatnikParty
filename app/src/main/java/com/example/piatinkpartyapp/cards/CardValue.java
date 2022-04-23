278ce36e6ae5f3decccc5f06e75db195aa9b0207
Alina <alinaeh@edu.aau.at>
2022-04-23 14:28:47 +0200

    as a random trump card should be used as swapping card of a schnopsndeck, CardValue was extended by a get randomized cardvalue functionality

--- app/src/main/java/com/example/piatinkpartyapp/cards/CardValue.java
+++ app/src/main/java/com/example/piatinkpartyapp/cards/CardValue.java
@@ -2,6 +2,8 @@ package com.example.piatinkpartyapp.cards;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
+
 /*Value a card can have*/
 public enum CardValue {
     SIEBEN,
@@ -12,4 +14,11 @@ public enum CardValue {
     OBER,
     KOENIG, //"Ö" not possible due to resource accessing
     ASS;
+    private static final List<CardValue> CARDVALUE_VALUES = Collections.unmodifiableList(Arrays.asList(values()));
+    private static final int SIZE = CARDVALUE_VALUES.size();
+    private static final Random RANDOM = new Random();
+
+    public static CardValue randomValue(){
+        return CARDVALUE_VALUES.get(RANDOM.nextInt(SIZE));
+    }
 }
