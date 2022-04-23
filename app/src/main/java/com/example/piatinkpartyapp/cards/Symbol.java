d35aead10c2cd22782866b899dd6bbe0b2721e5e
Alina <alinaeh@edu.aau.at>
2022-04-23 12:41:31 +0200

    Card-Symbols enum implemented

--- app/src/main/java/com/example/piatinkpartyapp/cards/Symbol.java
+++ app/src/main/java/com/example/piatinkpartyapp/cards/Symbol.java
@@ -1,4 +1,11 @@
 package com.example.piatinkpartyapp.cards;
-
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+/*Card Symbols*/
 public enum Symbol {
+    HERZ,
+    KARO,
+    KREUZ,
+    PICK;
 }
