b0405ff1de7d246ca3dc97c8c5f8bce8c5dd6967
NejcDolinsek <76431204+NejcDolinsek@users.noreply.github.com>
2022-04-25 16:14:51 +0200

    tried to fix crashing problem by deleting the problem class and implementing a new one.

--- app/src/main/java/com/example/piatinkpartyapp/screens/CreateGameFragment.java
+++ app/src/main/java/com/example/piatinkpartyapp/screens/CreateGameFragment.java
@@ -7,24 +7,38 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import com.example.piatinkpartyapp.R;
 
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link CreateGameFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class CreateGameFragment extends Fragment {
 
-public class CreateGameFragment extends Fragment implements View.OnClickListener{
-
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
+    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
-    public CreateGameFragment(){}
+    public CreateGameFragment() {
+        // Required empty public constructor
+    }
 
-    private Button ButtonBack;
-    private Button ButtonStartGame;
-
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment CreateGameFragment.
+     */
+    // TODO: Rename and change types and number of parameters
     public static CreateGameFragment newInstance(String param1, String param2) {
         CreateGameFragment fragment = new CreateGameFragment();
         Bundle args = new Bundle();
@@ -44,18 +58,9 @@ public class CreateGameFragment extends Fragment implements View.OnClickListener
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_create_game, container, false);
-    }
-
-    @Override
-    public void onClick(View view) {
-        if(view == ButtonBack){
-            getActivity().getSupportFragmentManager().beginTransaction().remove(this).commit();
-        }else if (view == ButtonStartGame){
-            // Go to Next Screen
-           // getActivity().getSupportFragmentManager().beginTransaction().replace(androidx.fragment.R. id. fragment_container_view_tag, new WaitingPlayersFragment()).commit();
-        }
     }
 }
--- app/src/main/java/com/example/piatinkpartyapp/screens/MainActivity.java
+++ app/src/main/java/com/example/piatinkpartyapp/screens/MainActivity.java
@@ -28,7 +28,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-
         if (view == CreateGameBtn) {
             showCreateGameFragment();
         }else if(view==PlayGameBtn){
