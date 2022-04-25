e090a6dc36dc246a39683fe183ac376c06afe8d6
Valon <valia.fruit10@gmail.com>
2022-04-25 16:34:40 +0200

    Added IPackets interface so it can be used placeholder for a generic packet send method in the GameClient class
    .sendTCP(packet) can only be called within a new Thread, else it would block the Main Thread, and throw an exception !
    Instead of doing :
    new Thread(()->{... code ...}).start(); every time we want to send a packet, we can just use client.sendPackage(IPacket p);
    
    Therefore I added a new function which allows us to pass a runnable
    which then will be handled by the ExecutorService.

--- app/src/main/java/com/example/piatinkpartyapp/networking/Packets.java
+++ app/src/main/java/com/example/piatinkpartyapp/networking/Packets.java
@@ -1,15 +1,45 @@
 package com.example.piatinkpartyapp.networking;
 
+// will be used as some kind of placeholder for a generic packet send method!
+interface IPackets{
+}
+
 
 /*
 NOTES :
     If you create a constructor, which also sets the variables,
-    then we also need an  empty constructor, else Kryonet throws an Exception !!!
+    then we also need an  empty constructor, else Kryonet throws an Exception !!!!
  */
 class Packets {
 
     public static class Requests{
+        public static class SendEndToEndChatMessage implements IPackets {
+            String message;
+            int from;
+            int to;
 
+            public SendEndToEndChatMessage() {
+            }
+
+            public SendEndToEndChatMessage(String message, int from, int to) {
+                this.message = message;
+                this.from = from;
+                this.to = to;
+            }
+        }
+
+        public static class SendToAllChatMessage {
+            String message;
+            int from;
+
+            public SendToAllChatMessage() {
+            }
+
+            public SendToAllChatMessage(String message, int from) {
+                this.message = message;
+                this.from = from;
+            }
+        }
     }
 
     public static class Responses {
@@ -25,9 +55,30 @@ class Packets {
             }
         }
 
-        //Empty message
-        public static class LobbyCreatedMessage {
-            public LobbyCreatedMessage(){
+        public static class ReceiveEndToEndChatMessage implements IPackets {
+            String message;
+            int from;
+            int to;
+
+            public ReceiveEndToEndChatMessage() {
+            }
+
+            public ReceiveEndToEndChatMessage(String message, int from, int to) {
+                this.message = message;
+                this.from = from;
+                this.to = to;
+            }
+        }
+        public static class ReceiveToAllChatMessage implements IPackets {
+            String message;
+            int from;
+
+            public ReceiveToAllChatMessage() {
+            }
+
+            public ReceiveToAllChatMessage(String message, int from) {
+                this.message = message;
+                this.from = from;
             }
         }
     }
