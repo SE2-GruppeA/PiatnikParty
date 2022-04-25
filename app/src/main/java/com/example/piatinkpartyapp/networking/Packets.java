8db58fc8375ee3a29e269c2667d61fd0d890a0ce
besenitza <besenitza@edu.aau.at>
2022-04-25 17:18:30 +0200

    -Adjustments after merge to dev Part 2

--- app/src/main/java/com/example/piatinkpartyapp/networking/Packets.java
+++ app/src/main/java/com/example/piatinkpartyapp/networking/Packets.java
@@ -1,22 +1,23 @@
 package com.example.piatinkpartyapp.networking;
+
 import com.example.piatinkpartyapp.cards.Card;
 
 import java.util.ArrayList;
 
 // will be used as some kind of placeholder for a generic packet send method!
 interface IPackets{
+    int x = 10;
 }
-
 
 
 /*
 NOTES :
     If you create a constructor, which also sets the variables,
-    then we also need an  empty constructor, else Kryonet throws an Exception !!!
+    then we also need an  empty constructor, else Kryonet throws an Exception !!!!
  */
-public class Packets {
-    // Requests -> Message From Client to Server
-    public static class Requests {
+class Packets {
+
+    public static class Requests{
         public static class SendEndToEndChatMessage implements IPackets {
             String message;
             int from;
@@ -32,25 +33,26 @@ public class Packets {
             }
         }
 
-        public static class StartGameMessage {
-            public StartGameMessage() {
-            }
-        }
-
         public static class SendToAllChatMessage {
             String message;
             int from;
 
             public SendToAllChatMessage() {
             }
+
+            public SendToAllChatMessage(String message, int from) {
+                this.message = message;
+                this.from = from;
+            }
+        }
+
+        public static class StartGameMessage {
+            public StartGameMessage() {
+            }
         }
     }
-    // Responses -> Message From Server to one Client or all Clients
+
     public static class Responses {
-        public void SendToAllChatMessage(String message, int from) {
-            this.message = message;
-            this.from = from;
-        }
         public static class ConnectedSuccessfully {
             int playerID;
             boolean isConnected;
@@ -89,7 +91,6 @@ public class Packets {
                 this.from = from;
             }
         }
-
         public static class SendHandCards {
             public int playerID;
             public ArrayList<Card> cards;
@@ -112,5 +113,4 @@ public class Packets {
             }
         }
     }
-
 }
