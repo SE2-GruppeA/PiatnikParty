31f29e87a494632f821935986b78221d7ec93490
tinesime <tine.simenc1@gmail.com>
2022-05-02 11:34:18 +0200

    MutableLiveData added, without test data.

---
+++ app/src/main/java/com/example/piatinkpartyapp/WaitingForPlayersViewModel.java
@@ -0,0 +1,46 @@
+package com.example.piatinkpartyapp;
+
+import android.os.CountDownTimer;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+public class WaitingForPlayersViewModel extends ViewModel {
+
+    private MutableLiveData<String> mutableLiveData;
+
+    public MutableLiveData<String> getIp() {
+        String ipAddress = null;
+
+        try {
+            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces();
+                 en.hasMoreElements();) {
+                NetworkInterface intf = en.nextElement();
+                for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses();
+                     enumIpAddr.hasMoreElements();) {
+                    InetAddress inetAddress = enumIpAddr.nextElement();
+                    if (!inetAddress.isLoopbackAddress()) {
+                        ipAddress = inetAddress.getHostAddress().toString();
+                    }
+                    if (ipAddress != null) {
+                        mutableLiveData = new MutableLiveData<>();
+                    }
+                }
+            }
+        } catch (SocketException ex) {
+            ex.printStackTrace();
+        }
+
+        return mutableLiveData;
+    }
+
+}
--- app/src/main/java/com/example/piatinkpartyapp/screens/WaitingPlayersFragment.java
+++ app/src/main/java/com/example/piatinkpartyapp/screens/WaitingPlayersFragment.java
@@ -3,18 +3,23 @@ package com.example.piatinkpartyapp.screens;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.piatinkpartyapp.R;
+import com.example.piatinkpartyapp.WaitingForPlayersViewModel;
 
 
 public class WaitingPlayersFragment extends Fragment {
 
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private TextView textView;
 
     private String mParam1;
     private String mParam2;
@@ -44,7 +49,17 @@ public class WaitingPlayersFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_waiting_players, container, false);
+
+        ViewModel viewModel = new ViewModelProvider(this).get(WaitingForPlayersViewModel.class);
+
+        String text = ((WaitingForPlayersViewModel) viewModel).getIp().toString();
+
+        textView = (TextView) view.findViewById(R.id.textView4);
+        textView.setText(text);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_waiting_players, container, false);
+        return view;
     }
 }
