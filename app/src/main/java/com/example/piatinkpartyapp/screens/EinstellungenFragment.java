4382a7ce91f7ebbaa74faf1d1c2061849cd36ee6
tinesime <tine.simenc1@gmail.com>
2022-05-07 17:31:50 +0200

    added feature to options: change screen brightness

--- app/src/main/java/com/example/piatinkpartyapp/screens/EinstellungenFragment.java
+++ app/src/main/java/com/example/piatinkpartyapp/screens/EinstellungenFragment.java
@@ -1,15 +1,26 @@
 package com.example.piatinkpartyapp.screens;
 
+import android.annotation.SuppressLint;
+import android.content.ContentResolver;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.provider.Settings;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.piatinkpartyapp.R;
 
@@ -20,9 +31,14 @@ import java.util.Objects;
  * Use the {@link EinstellungenFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class EinstellungenFragment extends Fragment implements View.OnClickListener {
+public class EinstellungenFragment extends Fragment implements View.OnClickListener, SeekBar.OnSeekBarChangeListener {
 
     private Button backBtn;
+    private SeekBar seekBar;
+    private ContentResolver contentResolver;
+    private Window window;
+    private int brightness;
+    private TextView percentageView;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -72,6 +88,26 @@ public class EinstellungenFragment extends Fragment implements View.OnClickListe
         backBtn = (Button) root.findViewById(R.id.button);
         backBtn.setOnClickListener(this);
 
+        seekBar = (SeekBar) root.findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(this);
+
+        percentageView = (TextView) root.findViewById(R.id.percentageView);
+
+        contentResolver = requireActivity().getContentResolver();
+
+        window = requireActivity().getWindow();
+
+        seekBar.setMax(100);
+
+        try {
+            brightness = Settings.System.getInt(contentResolver, Settings.System.SCREEN_BRIGHTNESS);
+        } catch (Settings.SettingNotFoundException e) {
+            Log.e("Error","Brightness cannot be changed");
+            e.printStackTrace();
+            Toast.makeText(getActivity().getApplicationContext(),
+                    "Brightness cannot be changed", Toast.LENGTH_SHORT).show();
+        }
+
         return root;
     }
 
@@ -80,4 +116,55 @@ public class EinstellungenFragment extends Fragment implements View.OnClickListe
         getActivity().getSupportFragmentManager().beginTransaction().remove(this).commit();
     }
 
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        Settings.System.canWrite(getContext());
+
+        if (progress <= 1) {
+            brightness = 1;
+        } else {
+            brightness = progress;
+        }
+
+        float percentage = (brightness / (float) 100) * 100;
+
+        percentageView.setText((int) percentage + " %");
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        // Nothing here
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        boolean grantPermissions = Settings.System.canWrite(getContext());
+        //Settings.ACTION_MANAGE_WRITE_SETTINGS;
+
+        if (!grantPermissions) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
+            startActivity(intent);
+
+            Toast.makeText(getActivity().getApplicationContext(),
+                    "The permissions could be granted", Toast.LENGTH_SHORT).show();
+
+            Toast.makeText(getActivity().getApplicationContext(),
+                    "Now you can change the screen brightness", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getActivity().getApplicationContext(),
+                    "Screen brightness changed to " + brightness, Toast.LENGTH_SHORT).show();
+
+            Log.e("Error", "The permissions could be granted");
+        }
+
+        Settings.System.putInt(contentResolver, Settings.System.SCREEN_BRIGHTNESS, brightness);
+
+        WindowManager.LayoutParams layoutParams = window.getAttributes();
+
+        layoutParams.screenBrightness = brightness / (float) 100;
+
+        window.setAttributes(layoutParams);
+    }
 }
