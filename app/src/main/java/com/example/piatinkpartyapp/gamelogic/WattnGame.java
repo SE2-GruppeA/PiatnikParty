a36d3f2c0db32210ea126c62b88ba3bf5f4e2061
Alina <alinaeh@edu.aau.at>
2022-05-08 23:09:59 +0200

    implemented tests for wattngame logic (deck with hit & trump), winning cases with right card - adaptions to WattnGame where needed since parts of code worked incorrectly

--- app/src/main/java/com/example/piatinkpartyapp/gamelogic/WattnGame.java
+++ app/src/main/java/com/example/piatinkpartyapp/gamelogic/WattnGame.java
@@ -1,4 +1,7 @@
 package com.example.piatinkpartyapp.gamelogic;
+import android.util.Log;
+import android.widget.Toast;
+
 import com.esotericsoftware.kryonet.Connection;
 import com.example.piatinkpartyapp.cards.Card;
 import com.example.piatinkpartyapp.cards.CardValue;
@@ -13,17 +16,17 @@ import java.util.ArrayList;
 import java.util.logging.Logger;
 public class WattnGame extends Game {
     private int mainPlayerId;
-    private Player roundStartPlayer;
-    private ArrayList<Player> players = new ArrayList<>();
-    private ArrayList<Integer> playerPoints = new ArrayList<>();
-    private WattnDeck deck;
+    public Player roundStartPlayer;
+   // public ArrayList<Player> players = new ArrayList<>();
+    public ArrayList<Integer> playerPoints = new ArrayList<>();
+    public WattnDeck deck;
 
 
 
     //logging for testing purposes
     private static final Logger LOG = Logger.getLogger(GameServer.class.getName());
 
-    public WattnGame(){}
+    public WattnGame(){resetWattnDeck();}
 
     @Override
     public void resetSchnopsnDeck(){}
@@ -31,12 +34,28 @@ public class WattnGame extends Game {
     public void resetWattnDeck(){
         deck = new WattnDeck(GameName.Wattn,2);
         //setting hit & trump randomly 4 testing because UI isn't connected to game logic via live data yet
-        deck.setHit(CardValue.randomValue());
-        deck.setTrump(Symbol.randomSymbol());
+       // deck.setHit(CardValue.randomValue());
+       // deck.setHit(CardValue.ZEHN);
+        //deck.setTrump(Symbol.HERZ);
+
+        //deck.rightCard = deck.getRightCard();
+      //  deck.setTrump(Symbol.randomSymbol());
         LOG.info("hit is "+ deck.getHit());
         LOG.info("trump is "+deck.getTrump());
+        playerPoints.add(0);
+        playerPoints.add(0);
     }
-
+    public CardValue setHit(CardValue cv){
+        this.deck.setHit(cv);
+        return cv;
+    }
+    public Symbol setTrump(Symbol s){
+        this.deck.setTrump(s);
+        return s;
+    }
+    public Card rightCard(){
+        return this.deck.getRightCard();
+    }
     @Override
     public Player getRoundWinnerPlayerSchnopsn(){return null;}
 
@@ -45,11 +64,15 @@ public class WattnGame extends Game {
         Player currentPlayer = getNextPlayer(this.roundStartPlayer);
         while (currentPlayer != this.roundStartPlayer) {
             //the player that plays the right card always wins the subround
-            if(currentPlayer.getCardPlayed() == deck.getRightCard()){
+           if(winningPlayer.getCardPlayed().getCardValue() == deck.getRightCard().getCardValue() && winningPlayer.getCardPlayed().getSymbol() == deck.getRightCard().getSymbol()){
+               winningPlayer = winningPlayer;
+           }
+           else if(currentPlayer.getCardPlayed().getSymbol() == deck.getRightCard().getSymbol() && currentPlayer.getCardPlayed().getCardValue() == deck.getRightCard().getCardValue()){
                 winningPlayer = currentPlayer;
 
             } //hit case - first played hit wins
             else if(currentPlayer.getCardPlayed().getCardValue() ==deck.getHit() && winningPlayer.getCardPlayed() != deck.getRightCard()){
+
                 winningPlayer = currentPlayer;
             } // trump case - higher trump wins
             else if(winningPlayer.getCardPlayed().getSymbol() == deck.getTrump()){
@@ -60,10 +83,15 @@ public class WattnGame extends Game {
             }else if(currentPlayer.getCardPlayed().getSymbol() == deck.getTrump() || (winningPlayer.getCardPlayed().getSymbol() == currentPlayer.getCardPlayed().getSymbol() && deck.cardPoints(currentPlayer.getCardPlayed().cardValue) > deck.cardPoints(winningPlayer.getCardPlayed().cardValue))){
                 winningPlayer = currentPlayer;
             }
-            Integer p = playerPoints.get(winningPlayer.getPoints());
+            Integer p = playerPoints.get(winningPlayer.getId()-1);
             p++;
-            playerPoints.set(winningPlayer.getId(),p);
+            playerPoints.set(winningPlayer.getId()-1,p);
             currentPlayer = getNextPlayer(currentPlayer);
+        }
+        for(Integer p : playerPoints){
+            if(p==3){
+                LOG.info(winningPlayer + " won this game!");
+            }
         }
         return winningPlayer;
     }
---
+++ app/src/test/java/com/example/piatinkpartyapp/WattnTest.java
@@ -0,0 +1,119 @@
+package com.example.piatinkpartyapp;
+
+import com.example.piatinkpartyapp.cards.Card;
+import com.example.piatinkpartyapp.cards.CardValue;
+import com.example.piatinkpartyapp.cards.Symbol;
+import com.example.piatinkpartyapp.gamelogic.Player;
+import com.example.piatinkpartyapp.gamelogic.WattnGame;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class WattnTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void setHitsetTrumptest(){
+        WattnGame wg = new WattnGame();
+     //   wg.deck.setHit(CardValue.ZEHN);
+       // wg.deck.setTrump(Symbol.HERZ);
+        wg.setHit(CardValue.ZEHN);
+        wg.setTrump(Symbol.HERZ);
+        assertEquals(CardValue.ZEHN, wg.deck.getHit());
+        assertEquals(Symbol.HERZ, wg.deck.getTrump());
+       // assertEquals(Symbol.HERZ, wg.deck.rightCard.symbol);
+        //assertEquals(CardValue.ZEHN,wg.deck.rightCard.cardValue);
+    }
+    @Test
+    public void getRightCard(){
+        WattnGame wg = new WattnGame();
+        wg.setHit(CardValue.KOENIG);
+        wg.setTrump(Symbol.PICK);
+        assertEquals(CardValue.KOENIG, wg.deck.getHit());
+        assertEquals(Symbol.PICK, wg.deck.getTrump());
+
+        assertEquals(CardValue.KOENIG, wg.rightCard().getCardValue());
+        assertEquals(Symbol.PICK, wg.rightCard().getSymbol());
+    }
+    @Test
+    public void testwithPlayersRightCard(){
+        Player p1 = new Player();
+        p1.setId(1);
+        Player p2 = new Player();
+        p2.setId(2);
+        WattnGame wg = new WattnGame();
+        wg.players.add(p1);
+        wg.players.add(p2);
+        wg.setHit(CardValue.KOENIG);
+        wg.setTrump(Symbol.PICK);
+
+        wg.roundStartPlayer = p1;
+
+        assertEquals(p1, wg.roundStartPlayer);
+        assertEquals(p2, wg.getNextPlayer(p1));
+
+        p1.setCardPlayed(new Card(Symbol.KARO,CardValue.ACHT));
+        p2.setCardPlayed(new Card(Symbol.PICK, CardValue.KOENIG));
+        Player winner = wg.getRoundWinnerWattn();
+        assertEquals(p2, winner);
+
+    }
+    @Test
+    public void testwithPlayersRight2(){
+        Player p1 = new Player();
+        p1.setId(1);
+        Player p2 = new Player();
+        p2.setId(2);
+        WattnGame wg = new WattnGame();
+        wg.players.add(p1);
+        wg.players.add(p2);
+        wg.setHit(CardValue.KOENIG);
+        wg.setTrump(Symbol.PICK);
+
+        wg.roundStartPlayer = p1;
+
+        assertEquals(p1, wg.roundStartPlayer);
+        assertEquals(p2, wg.getNextPlayer(p1));
+
+        p2.setCardPlayed(new Card(Symbol.HERZ,CardValue.KOENIG));
+        p1.setCardPlayed(new Card(Symbol.PICK, CardValue.KOENIG));
+        Player winner = wg.getRoundWinnerWattn();
+        assertEquals(p1, winner);
+
+    }
+    @Test
+    public void testwithPlayersHit1(){
+        Player p1 = new Player();
+        p1.setId(1);
+        Player p2 = new Player();
+        p2.setId(2);
+        WattnGame wg = new WattnGame();
+        wg.players.add(p1);
+        wg.players.add(p2);
+        wg.setHit(CardValue.KOENIG);
+        wg.setTrump(Symbol.PICK);
+
+        wg.roundStartPlayer = p1;
+
+        assertEquals(p1, wg.roundStartPlayer);
+        assertEquals(p2, wg.getNextPlayer(p1));
+
+        p1.setCardPlayed(new Card(Symbol.HERZ,CardValue.KOENIG));
+        p2.setCardPlayed(new Card(Symbol.PICK, CardValue.ASS));
+        Player winner = wg.getRoundWinnerWattn();
+        assertEquals(p1, winner);
+
+    }
+
+}
