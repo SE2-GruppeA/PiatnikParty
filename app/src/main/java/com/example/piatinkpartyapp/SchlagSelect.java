c6974a8caf35f314cd46c7f8d12258c2d4da510e
Anton <anton.serschen@gmail.com>
2022-05-28 13:08:39 +0200

    addes imagebuttons for chard values in SchlagSelect

--- app/src/main/java/com/example/piatinkpartyapp/SchlagSelect.java
+++ app/src/main/java/com/example/piatinkpartyapp/SchlagSelect.java
@@ -1,5 +1,6 @@
 package com.example.piatinkpartyapp;
 
+import android.media.Image;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,13 +8,23 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link SchlagSelect#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SchlagSelect extends Fragment {
+public class SchlagSelect extends Fragment implements View.OnClickListener {
+
+    ImageButton btnSieben;
+    ImageButton btnAcht;
+    ImageButton btnNeun;
+    ImageButton btnZehn;
+    ImageButton btnUnter;
+    ImageButton btnOber;
+    ImageButton btnKoenig;
+    ImageButton btnAss;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -59,6 +70,30 @@ public class SchlagSelect extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_schlag_select, container, false);
+        View view = inflater.inflate(R.layout.fragment_schlag_select, container, false);;
+
+        btnSieben = view.findViewById(R.id.btnSieben);
+        btnAcht = view.findViewById(R.id.btnAcht);
+        btnNeun = view.findViewById(R.id.btnNeun);
+        btnZehn = view.findViewById(R.id.btnZehn);
+        btnUnter = view.findViewById(R.id.btnUnter);
+        btnOber = view.findViewById(R.id.btnOber);
+        btnKoenig = view.findViewById(R.id.btnKoenig);
+        btnAss = view.findViewById(R.id.btnAss);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == btnSieben){
+
+        }else if(view == btnAcht){
+
+        }
+    }
+
+    public void setSchlag(){
+
     }
 }
