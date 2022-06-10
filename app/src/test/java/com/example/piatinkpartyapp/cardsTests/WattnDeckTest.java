24a06e357efdb9423dcebc19c910dfe3cb12d0f9
NejcDolinsek <76431204+NejcDolinsek@users.noreply.github.com>
2022-06-10 08:46:05 +0200

    Implemented a new Test Class for WattnDeck.java and wrote the necessary tests, forgot to add the class.

---
+++ app/src/test/java/com/example/piatinkpartyapp/cardsTests/WattnDeckTest.java
@@ -0,0 +1,88 @@
+package com.example.piatinkpartyapp.cardsTests;
+
+import com.example.piatinkpartyapp.cards.Card;
+import com.example.piatinkpartyapp.cards.CardValue;
+import com.example.piatinkpartyapp.cards.GameName;
+import com.example.piatinkpartyapp.cards.Symbol;
+import com.example.piatinkpartyapp.cards.WattnDeck;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class WattnDeckTest {
+
+    private WattnDeck deck;
+    private GameName gameName;
+    private Card card;
+
+    @BeforeEach
+    public void init(){
+        int players = 1;
+        gameName = GameName.Wattn;
+        card = new Card();
+        deck = new WattnDeck(gameName,players);
+    }
+
+    @AfterEach
+    public void clear(){
+        deck = null;
+        gameName = null;
+        card = null;
+    }
+
+    @Test
+    public void testLastCard(){
+        //Testing if not the last card in deck
+        deck.lastCard();
+        assertNotNull(deck);
+    }
+
+    @Test
+    public void testGetRightCard(){
+
+        //Testing if not the highest Value
+        card.cardValue = CardValue.ACHT;
+        card.symbol = Symbol.HERZ;
+
+        assertNull(deck.getRightCard());
+    }
+
+    @Test
+    public void testCardPoints(){
+        Card SIEBEN = new Card();
+        Card ACHT = new Card();
+        Card NEUN = new Card();
+        Card ZEHN = new Card();
+        Card UNTER = new Card();
+        Card OBER = new Card();
+        Card KOENIG = new Card();
+        Card ASS = new Card();
+
+        SIEBEN.cardValue = CardValue.SIEBEN;
+        ACHT.cardValue = CardValue.ACHT;
+        NEUN.cardValue = CardValue.NEUN;
+        ZEHN.cardValue = CardValue.ZEHN;
+        UNTER.cardValue = CardValue.UNTER;
+        OBER.cardValue = CardValue.OBER;
+        KOENIG.cardValue = CardValue.KOENIG;
+        ASS.cardValue = CardValue.ASS;
+
+        deck.cardPoints(SIEBEN.cardValue);
+        assertEquals(1,deck.cardPoints(SIEBEN.cardValue));
+        assertEquals(2,deck.cardPoints(ACHT.cardValue));
+        assertEquals(3,deck.cardPoints(NEUN.cardValue));
+        assertEquals(4,deck.cardPoints(ZEHN.cardValue));
+        assertEquals(5,deck.cardPoints(UNTER.cardValue));
+        assertEquals(6,deck.cardPoints(OBER.cardValue));
+        assertEquals(7,deck.cardPoints(KOENIG.cardValue));
+        assertEquals(8,deck.cardPoints(ASS.cardValue));
+    }
+
+
+
+}
